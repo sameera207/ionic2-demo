47700095227722dc5ff4c35e1bf05339c71a54e7
sameera <sameera207@gmail.com>
2016-03-01 22:53:13 +1100

    ionic2 demo project for ng-sydney

---
+++ app/pages/home/home.ts
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-framework/ionic';
+
+@Page({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+
+  constructor(){
+    this.name = 'ng-sydney';
+  }
+}
